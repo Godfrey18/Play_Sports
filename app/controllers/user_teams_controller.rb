69c433304f71dabb1dabeffda97823a9e7552f07
Godfrey18 <godfreymenezes07@gmail.com>
2020-03-16 17:38:51 +0530

    joining and existing from a team is complete

--- app/controllers/user_teams_controller.rb
+++ app/controllers/user_teams_controller.rb
@@ -1,6 +1,9 @@
 class UserTeamsController < ApplicationController
 	def create
      
+     @user = UserTeam.create(user_id: params[:user],team_id: params[:team])
+      flash[:sucess]="Welcome to new Team"
+      redirect_to team_path(params[:team])
 
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-has_many :user_teams
+has_many :user_teams,dependent: :destroy
 has_many :teams, through: :user_teams
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
