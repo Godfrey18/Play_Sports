6073aae4c8262dfcc6e48489a854f6342ccfc16f
Godfrey18 <godfreymenezes07@gmail.com>
2020-03-16 12:26:46 +0530

    team show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-
-	has_one :team
 	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
