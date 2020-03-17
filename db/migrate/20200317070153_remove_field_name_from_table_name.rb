949e0c69d2b3ac4b0b71eaac35972a5fdf043fe7
Godfrey18 <godfreymenezes07@gmail.com>
2020-03-17 12:39:36 +0530

    afretnonn changes

---
+++ db/migrate/20200317070153_remove_field_name_from_table_name.rb
@@ -0,0 +1,5 @@
+class RemoveFieldNameFromTableName < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :teams, :role, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_082146) do
+ActiveRecord::Schema.define(version: 2020_03_17_070153) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
